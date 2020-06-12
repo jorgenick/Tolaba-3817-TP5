9902fc0253b0519e6e503d760d6388823ee7be00
jorge <jorge@JORGEE>
2020-06-11 21:37:15 -0300

    2comentario de prueba autor: jorge tolaba

--- src/main/java/ar/edu/unju/fi/controller/MainCotroller.java
+++ src/main/java/ar/edu/unju/fi/controller/MainCotroller.java
@@ -1,5 +1,7 @@
 package ar.edu.unju.fi.controller;
-
+/**
+ * Comentario de prueba de HIt HUB 2do comentario
+ */
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
